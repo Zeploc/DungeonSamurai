693eef265582f98916b4f57dd58a27a8abbca8f5
Jontyhoekstra <Jontyhoekstra@gmail.com>
2018-08-07 17:47:22 +1200

    Fixed error in wombo

--- Assets/Scripts/Game/WomboCombo.cs
+++ Assets/Scripts/Game/WomboCombo.cs
@@ -23,23 +23,23 @@ public class WomboCombo : MonoBehaviour {
     {
         for (int i = 0; i < ComboCount; i++)
         {
-            QTEType = Random.Range(0, 4);
-            if (QTEType == 0)
-            {
-                QTEManagerRef.AddQTEToQueue("LeftJoystickLeft", 1, "LLeft", 1, 1, true, PlayerRef.LeftDodge);
-            }
-            if (QTEType == 1)
-            {
-                QTEManagerRef.AddQTEToQueue("LeftJoystickRight", 1, "LRight", 2, 0, true, PlayerRef.RightDodge);
-            }
-            if (QTEType == 2)
-            {
-                QTEManagerRef.AddQTEToQueue("RightJoystickLeft", 1, "RLeft", 3, 0, true, PlayerRef.LeftParray);
-            }
-            if (QTEType == 3)
-            {
-                QTEManagerRef.AddQTEToQueue("RightJoystickRight", 1, "RRight", 2, 0, true, PlayerRef.RightParray);
-            }
+            //QTEType = Random.Range(0, 4);
+            //if (QTEType == 0)
+            //{
+            //    QTEManagerRef.AddQTEToQueue("LeftJoystickLeft", 1, "LLeft", 1, 1, true, PlayerRef.LeftDodge);
+            //}
+            //if (QTEType == 1)
+            //{
+            //    QTEManagerRef.AddQTEToQueue("LeftJoystickRight", 1, "LRight", 2, 0, true, PlayerRef.RightDodge);
+            //}
+            //if (QTEType == 2)
+            //{
+            //    QTEManagerRef.AddQTEToQueue("RightJoystickLeft", 1, "RLeft", 3, 0, true, PlayerRef.LeftParray);
+            //}
+            //if (QTEType == 3)
+            //{
+            //    QTEManagerRef.AddQTEToQueue("RightJoystickRight", 1, "RRight", 2, 0, true, PlayerRef.RightParray);
+            //}
         }
     }
 }
